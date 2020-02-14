373c3157e53b7eddb1704a003cf70b39233ae84f
wjsassi <welson.sassi@hotmail.com>
2020-02-14 17:48:52 -0300

    Versao Final

--- DevFinderBack/src/controllers/DevController.js
+++ DevFinderBack/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const Dev = require('../models/dev');
 const parseStringAsArray = require('../utils/ParseStringAsArray');
+const { findConnections, sendMessage } = require('../websocket');
 
 module.exports = {
 
@@ -35,6 +36,17 @@ module.exports = {
                 techs: techsArray,
                 location,
             })
+
+            // Filtrar as conexões que estão há no máximo 10km de distância
+            // e que o novo dev tenha pelo menos uma das tecnologias filtradas
+
+            const sendSocketMessageTo = findConnections(
+            { latitude, longitude },
+                techsArray,
+            )
+
+            sendMessage(sendSocketMessageTo, 'new-dev', dev);
+
             return response.json(dev);
         }
         else {
@@ -42,11 +54,11 @@ module.exports = {
         }
     },
 
-    async update(){
+        async update(){
         // Atualizar dados... Name, avatar_url, bio, techs, location
     },
 
-    async destroy(){
+        async destroy(){
         //Excluir do Banco de Dados
     },
 };
